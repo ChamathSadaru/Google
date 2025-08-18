594b2eab99387076a339c2c94d77b3be0d5ef94a
CZ Gen (Chamath_Z) <czgenai@gmail.com>
2025-08-18 18:37:31 +0000

    // Import the functions you need from the SDKs you need import { initial

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPD7UlRwfd39G36Ojego_hNddfNcYLWtc",
+  authDomain: "cosmic-voyage-j93v6.firebaseapp.com",
+  projectId: "cosmic-voyage-j93v6",
+  storageBucket: "cosmic-voyage-j93v6.firebasestorage.app",
+  messagingSenderId: "371281393753",
+  appId: "1:371281393753:web:f697948d68b233acb61948"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export { app };
